202ac085edfec08908f28b8a0e1f24fb752fcf7a
4Raul <74277605+4Raul@users.noreply.github.com>
2021-09-06 21:03:23 -0300

    Create JJ's.lua

---
+++ JJ's.lua
@@ -0,0 +1,161 @@
+local plr = game.Players.LocalPlayer
+
+local args = {
+    [1] = "UM !",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+plr.Character:FindFirstChildOfClass("Humanoid").Jump = true
+wait(2)
+local args = {
+    [1] = "DOIS !",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+plr.Character:FindFirstChildOfClass("Humanoid").Jump = true
+wait(2)
+local args = {
+    [1] = "TRÊS !",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+plr.Character:FindFirstChildOfClass("Humanoid").Jump = true
+wait(2)
+local args = {
+    [1] = "QUATRO !",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+plr.Character:FindFirstChildOfClass("Humanoid").Jump = true
+wait(2)
+local args = {
+    [1] = "CINCO !",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+plr.Character:FindFirstChildOfClass("Humanoid").Jump = true
+wait(2)
+local args = {
+    [1] = "SEIS !",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+plr.Character:FindFirstChildOfClass("Humanoid").Jump = true
+wait(2)
+local args = {
+    [1] = "SETE !",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+plr.Character:FindFirstChildOfClass("Humanoid").Jump = true
+wait(3)
+local args = {
+    [1] = "OITO !",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+plr.Character:FindFirstChildOfClass("Humanoid").Jump = true
+wait(2)
+local args = {
+    [1] = "NOVE !",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+plr.Character:FindFirstChildOfClass("Humanoid").Jump = true
+wait(2)
+local args = {
+    [1] = "DEZ !",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+plr.Character:FindFirstChildOfClass("Humanoid").Jump = true
+wait(2)
+local args = {
+    [1] = "ONZE !",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+plr.Character:FindFirstChildOfClass("Humanoid").Jump = true
+wait(2)
+local args = {
+    [1] = "DOZE !",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+plr.Character:FindFirstChildOfClass("Humanoid").Jump = true
+wait(2)
+local args = {
+    [1] = "TREZE !",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+plr.Character:FindFirstChildOfClass("Humanoid").Jump = true
+wait(3)
+local args = {
+    [1] = "CATORZE !",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+plr.Character:FindFirstChildOfClass("Humanoid").Jump = true
+wait(2)
+local args = {
+    [1] = "QUINZE !",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+plr.Character:FindFirstChildOfClass("Humanoid").Jump = true
+wait(3)
+local args = {
+    [1] = "DEZESSEIS !",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+plr.Character:FindFirstChildOfClass("Humanoid").Jump = true
+wait(2)
+local args = {
+    [1] = "DEZESSETE !",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+plr.Character:FindFirstChildOfClass("Humanoid").Jump = true
+wait(2)
+local args = {
+    [1] = "DEZOITO !",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+plr.Character:FindFirstChildOfClass("Humanoid").Jump = true
+wait(2)
+local args = {
+    [1] = "DEZENOVE !",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+plr.Character:FindFirstChildOfClass("Humanoid").Jump = true
+wait(2)
+local args = {
+    [1] = "VINTE !",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+plr.Character:FindFirstChildOfClass("Humanoid").Jump = true
